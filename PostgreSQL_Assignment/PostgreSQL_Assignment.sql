3f4e643bcac071eebfffe0edf1e91e6847fd3d12
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-05-26 21:18:50 +0600

    created table and insert data

--- PostgreSQL_Assignment/PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment/PostgreSQL_Assignment.sql
@@ -51,6 +51,17 @@ VALUES
     ('Sloth Bear', 'Melursus ursinus', '1791-01-01', 'Vulnerable'),
     ('Indian Star Tortoise', 'Geochelone elegans', '1831-01-01', 'Vulnerable');
 
+INSERT INTO sightings(species_id, ranger_id, location, sighting_time, notes)
+    VALUES 
+        (1, 1, 'Peak Ridge', '2024-05-10 07:45:00', 'Camera trap image captured'),
+        (2, 2, 'Bankwood Area', '2024-05-12 16:20:00', 'Juvenile seen'),
+        (2, 3, 'Bamboo Grove East', '2024-05-15 09:10:00', 'Feeding observed'),
+        (1, 2, 'Snowfall Pass', '2024-05-18 18:30:00', 'Camera trap image captured')
 
 
-SELECT * from rangers
+
+
+
+SELECT * from rangers;
+SELECT * FROM species;
+SELECT * FROM sightings;
